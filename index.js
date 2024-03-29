3a378fc6562d0f8414214a4e6a7473df79ba2b80
GoodGuy1234567 <165426230+GoodGuy1234567@users.noreply.github.com>
2024-03-29 22:39:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const C = document.getElementById("C");
+const Cr = document.getElementById("Cr");
+const F = document.getElementById("F");
+const Fr = document.getElementById("Fr");
+const K = document.getElementById("K");
+const Kr = document.getElementById("Kr");
+const TextInput = document.getElementById("textInput");
+const Submit = document.getElementById("submit");
+const results = document.getElementById("results")
+let first;
+let second;
+
+Submit.onclick = function(){
+    if(C.checked){
+        first = 1
+    }else if(F.checked){
+        first = 2
+    }else if(K.checked){
+        first = 3
+    }else{
+        results.textContent = "You didn't choose something"
+    };
+    if(Cr.checked){
+        second = 1
+    }else if(Fr.checked){
+        second = 2
+    }else if(Kr.checked){
+        second = 3
+    }else{
+        results.textContent = "You didn't choose something"
+    };
+    if(first == second){
+        results.textContent = "You can't pick the same units of measurement or you didn't choose them"
+    }else if(first == 1 && second == 2){
+        results.textContent = TextInput.value * 1.8 + 32 + " ℉"
+    }else if(first == 1 && second == 3){
+        results.textContent = TextInput.value + 273.15 + " K"
+    }else if(first == 2 && second == 1){
+        results.textContent = (TextInput.value - 32) * 5 / 9  + " ℃"
+    }else if(first == 2 && second == 3){
+        results.textContent = (TextInput.value - 32) / 1.8 + 273.15 + " K"
+    }else if(first == 3 && second == 1){
+        results.textContent = TextInput.value - 273.15 + " ℃"
+    }else if(first == 3 && second == 2){
+        results.textContent = (TextInput.value - 273.15) * 1.8 + 32 + " ℉"
+    }
+}
